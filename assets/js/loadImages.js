9d321c4939da94d501a1e2f29fcc5b1c432ef28b
PeiwenDu <43226734+PeiwenDu@users.noreply.github.com>
2023-03-26 19:38:32 -0400

    first draft

--- assets/js/loadImages.js
+++ assets/js/loadImages.js
@@ -1,18 +1,13 @@
 $(document).ready(function() {
-    const repoUrl = 'https://api.github.com/repos/PeiwenDu/PeiwenDu.github.io/contents/assets/img/';
-    $.getJSON(repoUrl, function(data) {
-        $.each(data, function(i, item) {
-            if (item.type === 'file' && item.name.match(/\.(jpe?g|png|gif)$/)) {
-                const imageUrl = item.download_url;
-                var img;
-                console.log(i);
-                if (i == 0) {
-                    img = $('<img>').attr('src', imageUrl).attr('id', 'slideimg' + i).addClass('slide').addClass('showMe');
-                } else {
-                    img = $('<img>').attr('src', imageUrl).attr('id', 'slideimg' + i).addClass('slide');
+    $.ajax({
+        url: 'img/',
+        success: function(data) {
+            $(data).find('a').attr('href', function(index, value) {
+                if (value.match(/\.(jpe?g|png|gif)$/)) {
+                    const img = $('<img>').attr('src', 'images/' + value).addClass('slide');
+                    $('.containerSlider').append(img);
                 }
-                $('.containerSlider').append(img);
-            }
-        });
+            });
+        }
     });
 });
