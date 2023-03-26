cb6548ba0e809e91b5a7a46a492c493ab6f49e18
PeiwenDu <43226734+PeiwenDu@users.noreply.github.com>
2023-03-26 13:19:09 -0400

    Update loadImages.js

--- assets/js/loadImages.js
+++ assets/js/loadImages.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: '../assets/img/',
+        url: '/assets/img/',
         success: function(data) {
             $(data).find('a').attr('href', function(index, value) {
                 if (value.match(/\.(jpe?g|png|gif)$/)) {
