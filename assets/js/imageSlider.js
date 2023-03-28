047198f317b7ba237a536a87f7802d1b1cf84500
PeiwenDu <43226734+PeiwenDu@users.noreply.github.com>
2023-03-27 22:14:21 -0400

    change photo adaptive

--- assets/js/imageSlider.js
+++ assets/js/imageSlider.js
@@ -1,6 +1,6 @@
 var timer = setInterval(nextImage, 8000);
 let curImage = 0;
-//edit number of images here
+    //edit number of images here
 let numImages = 3;
 
 function nextImage() {
@@ -11,13 +11,13 @@ function nextImage() {
 
     // compute next image
     curImage++;
-    if (curImage > numImages - 1) {
-        curImage = 0;
+   if (curImage > numImages - 1) {
+       curImage = 0;
     }
 
     // add showMe class to next image
     e = document.getElementById("slideimg" + curImage);
-    addClass(e, "showMe");
+     addClass(e, "showMe");
 
 }
 
@@ -30,37 +30,37 @@ function prevImage() {
 
     // compute next image
     curImage--;
-    if (curImage < 0) {
-        curImage = numImages - 1;
+   if (curImage < 0) {
+       curImage = numImages - 1;
     }
 
     // add showMe class to next image
     e = document.getElementById("slideimg" + curImage);
 
-    addClass(e, "showMe");
+     addClass(e, "showMe");
 
 }
 
 
 function addClass(elem, name) {
     var c = elem.className;
-    if (c) c += " "; // if not blank, add a space separator
+    if (c) c += " ";  // if not blank, add a space separator
     c += name;
     elem.className = c;
 }
 
 function removeClass(elem, name) {
     var c = elem.className;
-    elem.className = c.replace(name, "").replace(/\s+/g, " ").replace(/^\s+|\s+$/g, ""); // remove name and extra blanks
+    elem.className = c.replace(name, "").replace(/\s+/g, " ").replace(/^\s+|\s+$/g, "");  // remove name and extra blanks
 }
 
 $(document).ready(function() {
-    $('.arrowLeft').click(function() {
-        prevImage();
-    });
-    $('.arrowRight').click(function() {
-        nextImage();
-    });
+  $('.arrowLeft').click(function() {
+    prevImage();
+  });
+$('.arrowRight').click(function() {
+    nextImage();
+  });
 });
 
 $(document).ready(function() {
